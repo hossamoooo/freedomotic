aac1dc4d6bac118a8501ac41b025cf204aa8419f
Mauro Cicolella <mcicolella@libero.it>
2017-03-11 18:48:33 +0100

    updated plugin configuration

--- plugins/devices/webiopi/src/main/java/com/freedomotic/plugins/devices/webiopi/WebIOPi.java
+++ plugins/devices/webiopi/src/main/java/com/freedomotic/plugins/devices/webiopi/WebIOPi.java
@@ -39,7 +39,13 @@ import org.json.JSONObject;
 public class WebIOPi extends Protocol {
 
     private static final Logger LOG = LoggerFactory.getLogger(WebIOPi.class.getName());
+    private final String USERNAME = configuration.getStringProperty("username", "webiopi");
+    private final String PASSWORD = configuration.getStringProperty("password", "raspberry");
+    private final String RASPBERRY_IP = configuration.getStringProperty("raspberry-ip-address", "127.0.0.1");
+    private final Integer RASPBERRY_PORT = configuration.getIntProperty("raspberry-ip-port", 8000);
+
     private HttpHelper httpHelper;
+
     String url = "https://private-anon-857d7cc727-zwayhomeautomation.apiary-mock.com/ZAutomation/api/v1/devices";
 
     String url2 = "{\"UART0\": 1, \"I2C0\": 0, \"I2C1\": 1, \"SPI0\": 0, \"GPIO\":{\n"
@@ -76,13 +82,15 @@ public class WebIOPi extends Protocol {
 
     @Override
     protected void onRun() {
-        //try {
-        //String content = httpHelper.retrieveContent(url);
-        //sendChanges(content);
-        sendChanges(url2);
-        //} catch (IOException ex) {
-        //    java.util.logging.Logger.getLogger(WebIOPi.class.getName()).log(Level.SEVERE, null, ex);
-        //}
+        try {
+
+            String content = httpHelper.retrieveContent("http://" + RASPBERRY_IP + ":" + RASPBERRY_PORT + "/", USERNAME, PASSWORD);
+            //sendChanges(content);
+            //sendChanges(url2);
+            LOG.info("Data received from Raspberry Pi \"" + content + "\"");
+        } catch (IOException ex) {
+            LOG.error(ex.getLocalizedMessage());
+        }
     }
 
     private void sendChanges(String content) {
@@ -92,25 +100,23 @@ public class WebIOPi extends Protocol {
             JSONObject gpio = obj.getJSONObject("GPIO");
             JSONObject current = gpio.getJSONObject("0");
             System.out.println(current.getString("function"));
-            System.out.println(current.getInt("value"));   
+            System.out.println(current.getInt("value"));
         }
-
-    
-
-}
-
-@Override
-        protected void onCommand(Command c) throws UnableToExecuteException {
 
     }
 
     @Override
-        protected boolean canExecute(Command c) {
+    protected void onCommand(Command c) throws UnableToExecuteException {
+
+    }
+
+    @Override
+    protected boolean canExecute(Command c) {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-        protected void onEvent(EventTemplate event) {
+    protected void onEvent(EventTemplate event) {
         //not nothing. This plugins doesn't listen to freedomotic events
     }
 }
